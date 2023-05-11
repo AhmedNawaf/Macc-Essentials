a06da0090b46f1dbcfc078483a732de97cc2b5e1
sa23sa <74133458+sa23sa@users.noreply.github.com>
2023-05-11 19:02:46 +0300

    accordions and filter buttons including reset button and their functionality has been added

--- src/components/collection/ProductsSection.tsx
+++ src/components/collection/ProductsSection.tsx
@@ -1,7 +1,23 @@
-import { Grid, GridItem, Accordion } from '@chakra-ui/react';
+import { Grid, GridItem, Accordion, HStack, Button } from '@chakra-ui/react';
+import { useSearchParams } from 'react-router-dom';
 import CustomAccordion from '../CustomAccordion';
 
 function ProductsSection() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const filterType = searchParams.get('type');
+
+  function handleSearchParams(value: string | null) {
+    setSearchParams((prevSearchParams) => {
+      if (!value) {
+        prevSearchParams.delete('type');
+      } else {
+        prevSearchParams.set('type', value);
+      }
+
+      return prevSearchParams;
+    });
+  }
+
   return (
     <Grid
       templateRows='repeat(2, 1fr)'
@@ -16,7 +32,43 @@ function ProductsSection() {
           <CustomAccordion title='Categories'>This is test</CustomAccordion>
         </Accordion>
       </GridItem>
-      <GridItem colSpan={4} bg='tomato' mt='6' />
+      <GridItem colSpan={4} mt='6'>
+        <HStack ms='12' spacing='12'>
+          {['Filter 1', 'Filter 2', 'Filter 3', 'Reset'].map((name) => {
+            if (name === 'Reset') {
+              if (filterType)
+                return (
+                  <Button
+                    key={name}
+                    py='6'
+                    px='12'
+                    size='lg'
+                    onClick={() =>
+                      handleSearchParams(name === 'Reset' ? null : name)
+                    }
+                  >
+                    {name}
+                  </Button>
+                );
+              else return null;
+            }
+            return (
+              <Button
+                key={name}
+                py='6'
+                px='12'
+                size='lg'
+                colorScheme={`${filterType === name ? 'orange' : 'gray'}`}
+                onClick={() =>
+                  handleSearchParams(name === 'Reset' ? null : name)
+                }
+              >
+                {name}
+              </Button>
+            );
+          })}
+        </HStack>
+      </GridItem>
     </Grid>
   );
 }
